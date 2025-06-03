917664b2905bac246d777bc683259b0813a3c513
xkubax242 <kuba242@interia.pl>
2025-06-03 21:04:17 +0200

    poprawki

--- example-app/routes/web.php
+++ example-app/routes/web.php
@@ -3,17 +3,26 @@
 use Illuminate\Support\Facades\Route;
 use Livewire\Volt\Volt;
 use App\Models\Restaurant;
-
+use App\Http\Controllers\RestaurantController;
 Route::get('/', function () {
+
 
     $restaurants = Restaurant::all();
 
     return view('welcome', compact('restaurants'));
 })->name('home');
 
+
+
 Route::get('/restaurant/{id}', function ($id) {
-    return "Strona restauracji o ID: " . $id;
+    $restaurant = \App\Models\Restaurant::findOrFail($id);
+
+    $reviews = $restaurant->reviews()->latest()->get();
+    return view('restaurant', compact('restaurant', 'reviews'));
+
 });
+
+
 
 Route::view('dashboard', 'dashboard')
     ->middleware(['auth', 'verified'])
@@ -34,32 +43,5 @@ Route::middleware(['auth'])->group(function () {
     Volt::route('settings/password', 'settings.password')->name('settings.password');
     Volt::route('settings/appearance', 'settings.appearance')->name('settings.appearance');
 });
-
-Route::prefix('cart')->group(function () {
-    Route::post('/add/{menuItemId}', [CartController::class, 'add'])->name('cart.add');
-    Route::post('/remove/{restaurantId}/{menuItemId}', [CartController::class, 'remove'])->name('cart.remove');
-    Route::post('/clear', [CartController::class, 'clear'])->name('cart.clear');
-    Route::get('/', [CartController::class, 'show'])->name('cart.show');
-});
-
-// TESTOWO DO DODAWANIA DO CARTA
-
-use App\Models\MenuItem;
-use App\Services\CartService;
-
-Route::get('/test-add-to-cart/{id}/{quantity?}', function ($id, $quantity = 1, CartService $cart) {
-    $item = MenuItem::with('restaurant')->find($id);
-    
-    if (!$item) {
-        return "Menu item not found";
-    }
-
-    $quantity = max(1, (int)$quantity);
-    $cart->add($item, $quantity);
-    
-    return "Added: {$item->name} (Quantity: {$quantity})";
-});
-
-// TESTOWO CART ^^^^
 
 require __DIR__.'/auth.php';
